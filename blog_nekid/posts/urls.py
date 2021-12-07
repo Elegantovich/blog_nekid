04f0307bd760081ff7267e565705a3296d42bf8d
Elegantovich <kh.msm@yandex.ru>
2021-12-07 20:55:03 +0300

    save

--- blog_nekid/posts/urls.py
+++ blog_nekid/posts/urls.py
@@ -6,21 +6,14 @@ app_name = 'posts'
 urlpatterns = [
     path('', views.index, name='index'),
     path('blog/<str:username>/', views.blog, name='blog'),
+    path('blog/<str:username>/follow/', views.blog_follow, name='blog_follow'),
+    path('blog/<str:username>/unfollow/', views.blog_unfollow,
+         name='blog_unfollow'),
     path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('follow/', views.follow_index, name='follow_index'),
     path('create/', views.post_create, name='post_create'),
     path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
-    path('follow/', views.follow_index, name='follow_index'),
-    path('blog/<str:username>/follow/',
-         views.blog_follow,
-         name='blog_follow'),
-    path('blog/<str:username>/unfollow/',
-         views.blog_unfollow,
-         name='blog_unfollow'),
-    path('posts/<int:post_id>/viewed/',
-         views.post_viewed,
-         name='post_viewed'),
-    path('posts/<int:post_id>/unviewed/',
-         views.post_unviewed,
+    path('posts/<int:post_id>/viewed/', views.post_viewed, name='post_viewed'),
+    path('posts/<int:post_id>/unviewed/', views.post_unviewed,
          name='post_unviewed')
          ]
-
